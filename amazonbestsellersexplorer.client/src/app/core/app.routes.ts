624daf7ccb408eb094dc93460f1fcdebb0284579
Damian Frisch <damian.frisch@gmail.com>
2026-04-25 18:58:39 +0200

    Add basic Angular structure

---
+++ amazonbestsellersexplorer.client/src/app/core/app.routes.ts
@@ -0,0 +1,17 @@
+﻿// File: src/app/core/app.routes.ts
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('../features/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('../features/auth/register/register.component').then(m => m.RegisterComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('../features/auth/login/login.component').then(m => m.LoginComponent)
+  }
+];
