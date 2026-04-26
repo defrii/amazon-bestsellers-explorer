1aa24369fedce6c22b81ee2a268e1aab01b66bf6
Damian Frisch <damian.frisch@gmail.com>
2026-04-26 17:31:16 +0200

    Add animations for page switching

--- amazonbestsellersexplorer.client/src/app/core/app.config.ts
+++ amazonbestsellersexplorer.client/src/app/core/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
@@ -11,7 +11,7 @@ import { jwtInterceptor } from './jwt.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(withInterceptors([jwtInterceptor])),
     provideAnimationsAsync(),
     providePrimeNG({
